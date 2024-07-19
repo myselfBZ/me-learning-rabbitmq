66fd80b184c194c6bbdf9001d72c32c50256afe6
Boburmirzo <symonbobur@gmail.com>
2024-07-19 22:15:52 +0500

    Initial commit

---
+++ basics/consumer/main.go
@@ -0,0 +1,88 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"time"
+
+	"github.com/streadway/amqp"
+)
+
+
+type Order struct{
+    ID          string  `json:"id"`
+    CutomerID   string  `json:"customerId"`
+    ItemID      string  `json:"itemId"` 
+    Quantity    int     `json:"quantity"`
+}
+
+
+
+func Consume(ch *amqp.Channel, q amqp.Queue)(){
+    defer ch.Close()
+    msgs, _ := ch.Consume(
+        q.Name,
+        "",
+        false,
+        false,
+        false,
+        false,
+        nil,
+        )
+    if msgs == nil{
+        log.Print("Nil channel")
+    }
+    go func ()  {
+        ProcessOrders(msgs)
+    }()    
+}
+
+
+func ProcessOrders(msgs <-chan amqp.Delivery)  {
+    for o := range msgs{
+        log.Print("Processing the order....")
+        time.Sleep(time.Second)
+        var order Order
+        err := json.Unmarshal(o.Body, &order)
+        if err != nil {
+            log.Println("Error serializing the json")
+        }
+        log.Printf("OrderId:%s, CustomerId:%s, Item: %s, Quantity:%d", order.ID, order.CutomerID, order.ItemID, order.Quantity)
+        
+    }
+    log.Print("Done processing the messages")
+}
+
+func FailOnErr(err error, msg string)  {
+    if err != nil{
+        log.Fatal(err, msg)
+    }
+}
+
+
+
+
+
+func main()  {
+    conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
+    defer conn.Close()
+    FailOnErr(err,  "Connection error")
+    log.Print("Connected")
+    ch, err := conn.Channel()
+    FailOnErr(err, "Channel creation error")
+    log.Print("We have a channel")
+    q, err := ch.QueueDeclare(
+        "orders",
+        false,
+        false,
+        false,
+        false,
+        nil,
+
+        )
+    FailOnErr(err, "Queue declaration error")
+    Consume(ch, q)
+    forever := make(chan bool)
+    
+    <-forever
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module me_learning_rabbiqmq
+
+go 1.22.5
+
+require github.com/streadway/amqp v1.1.0 // indirect
