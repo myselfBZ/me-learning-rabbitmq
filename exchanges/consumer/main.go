6797691399a9d17e258dfc4fffc26d7ce42d2155
Boburmirzo <symonbobur@gmail.com>
2024-07-21 09:55:29 +0500

    Consuming from many queues

--- exchanges/consumer/main.go
+++ exchanges/consumer/main.go
@@ -3,38 +3,56 @@ package main
 import (
 	"log"
 	"me_learning_rabbiqmq/errs"
+	"sync"
 
 	"github.com/streadway/amqp"
 )
 
+var wg = sync.WaitGroup{}
+
+
 func main()  {
     conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
     FailOnErr(err, errs.ConnErr)
+    queues := []string{"queue1", "queue2", "queue3",}
     defer conn.Close()
+    forever := make(chan struct{})
+    for _, q := range queues{
+        go Consume(conn, q, &wg)
+    } 
+    <-forever
+
+
+}
+
+
+
+func Consume(conn *amqp.Connection, queue string,wg *sync.WaitGroup)  {
+    defer wg.Done()
     ch, err := conn.Channel()
-    FailOnErr(err, errs.ChannelErr)
     defer ch.Close()
+    FailOnErr(err, errs.ChannelErr)
     msgs, err := ch.Consume(
-        "orders",
+        queue, 
         "",
         true,
         false,
         false,
         false,
         nil,
-        
-        )
-    FailOnErr(err, "You are actually bad at this")
-    forever := make(chan struct{})
-    go func(){
-        for m := range msgs{
-            log.Print(string(m.Body))
-        }
-    }()
-    <-forever
 
+        )
+    FailOnErr(err, "Failed to register a consumer")
+    log.Printf("Waiting for messages from %s", queue)
+    for m := range msgs{
+        log.Printf("Message coming from %s: %s", queue, string(m.Body))
+    }
 
 }
+
+
+
+
 
 func FailOnErr(err error, msg string)  {
     if err != nil{
